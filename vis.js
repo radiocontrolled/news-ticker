ced24a4c69dc101a888c7aa99d6a9010c8a1810d
radiocontrolled <radiocontrolled@gmail.com>
2014-04-03 16:46:38 -0700

    make a request via jsonp to guardian content api

--- vis.js
+++ vis.js
@@ -7,10 +7,30 @@ var svg = d3.select("article")
 				width:w,
 				height:h
 			})
-			.append("text")
-			.text("foo")
-			.attr({
-				"x":100,
-      			"y":100
-			})
 		
+//get the latest Guardian articles 
+d3.jsonp("http://content.guardianapis.com/search?&api-key=qcpra5vyaq7srabthtyvhhxs&callback=doVis"); 
+
+function doVis(data){
+				
+	var unorderedList = svg.selectAll("text")
+		.data(data.response.results)
+		.enter()
+		.append("text")
+		.attr({
+			"href": function(d){
+				return d.webUrl;
+			},
+			"x":100,
+      		"y":100
+		})
+		.text(function(d){
+			return d.webTitle;
+		})	
+	
+
+};
+
+
+
+	
